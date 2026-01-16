616e0609302faf3de3a326df78c931f2861bebed
Mahmoud Ebrahim <142084714+B-MEbrahim@users.noreply.github.com>
2026-01-16 15:16:14 +0000

    ch5

---
+++ schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE league (
+    league_id INTEGER NOT NULL,
+    league_name VARCHAR NOT NULL,
+    scoring_type VARCHAR NOT NULL,
+    last_changed_date DATE NOT NULL,
+    PRIMARY KEY (league_id)
+);
+
+CREATE TABLE team(
+    team_id INTEGER NOT NULL,
+    team_name VARCHAR NOT NULL,
+    league_id INTEGER NOT NULL,
+    last_changed_date DATE NOT NULL,
+    PRIMARY KEY (team_id),
+    FOREIGN KEY (league_id) REFERENCES league(league_id)
+);
+
+CREATE TABLE team_player(
+    team_id INTEGER NOT NULL,
+    player_id INTEGER NOT NULL,
+    last_changed_date DATE NOT NULL,
+    PRIMARY KEY (team_id, player_id),
+    FOREIGN KEY (team_id) REFERENCES team (team_id),
+    FOREIGN KEY (player_id) REFERENCES player (player_id)
+);
+
